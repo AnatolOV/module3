d756c82158cdd5caa56a01167dbc49fa6b7c8882
Anatoliy Oleynikov <oleinikovav.ao@gmail.com>
2024-07-14 11:25:27 +0300

    task4

--- main.py
+++ main.py
@@ -1,4 +1,21 @@
-def print_(a, b, c = 1):
-    print(a, b, c)
+# def print_params(args):
+#     for key, value in args.items():
+#         print(key)
+#         print(value)
+#     print('аргументы: ', args)
+# dict_ = {'e':4, 'r':5}
+# print_params(dict_)
+# def summutor(*args, name="wed"):
+#     s = 0
+#     for i in args:
+#         s += i
+#     print(s)
+#     print(name)
+#
+# summutor(1,2,3,5, name="d")
 
-print_(3, b =4, c = 3)
+def print_(a):
+    print(a)
+list_ = {'s':3,'f':5,'r': 6}
+# print_(1,5)
+print_(list_)
